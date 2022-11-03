e5242999a92221ac57e378fe44da77b54034ac1b
Leandro Almeida <leandrodev17@gmail.com>
2022-11-02 23:41:53 -0300

    02/11/2022 - Feat: Teste Aws CI/CD

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import {Request, Response} from 'express';
 const routers = express.Router()
 
 routers.get('/teste', (req: Request, res: Response) => {
-    return res.json({'SUCESS': 'FOI'})
+    return res.json({'SUCESS': 'FOI atualizado'})
 })
 
 module.exports = routers 
